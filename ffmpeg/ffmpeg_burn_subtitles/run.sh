2408c134c6335cd50cf3d33a73faf7e948639566
Larry Moore <hvw@yandex.com>
2020-03-23 15:50:12 +0800

    ffmpeg_burn_subtitles

--- ffmpeg/ffmpeg_burn_subtitles/run.sh
+++ ffmpeg/ffmpeg_burn_subtitles/run.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-# curl -o run.sh http://raw.githubusercontent.com/gubnota/fenki_shell_helpers/master/ffmpeg/ffmpeg_burn_subtitles/run.sh; bash run.sh
+# curl -O https://raw.githubusercontent.com/gubnota/fenki_shell_helpers/master/ffmpeg/ffmpeg_burn_subtitles/run.sh; bash run.sh
 cd "$(dirname $0)";
 cwd=${PWD##*/}          # to assign to a variable
 if [ 'ffmpeg_burn_subtitles' != $cwd ]; then
 echo "Creating a directory ffmpeg_burn_subtitles … Done!";
-mkdir ffmpeg_burn_subtitles; cd ffmpeg_burn_subtitles; cwd=${PWD##*/}
+if [ ! -d ffmpeg_burn_subtitles ]; then
+mkdir ffmpeg_burn_subtitles; 
+fi
+cd ffmpeg_burn_subtitles; cwd=${PWD##*/}
 fi
 
 if [ ! -f run.sh ]; then
-    curl -o 'run.sh' "http://raw.githubusercontent.com/gubnota/fenki_shell_helpers/master/ffmpeg/ffmpeg_burn_subtitles/run.sh";
+    curl -O "https://raw.githubusercontent.com/gubnota/fenki_shell_helpers/master/ffmpeg/ffmpeg_burn_subtitles/run.sh";
     chmod a+x run.sh;
 fi
 if [ ! -f Screen.png ]; then
-    curl -o 'Screen.png' "http://raw.githubusercontent.com/gubnota/fenki_shell_helpers/master/ffmpeg/ffmpeg_burn_subtitles/Screen.png";
+    curl -O "https://raw.githubusercontent.com/gubnota/fenki_shell_helpers/master/ffmpeg/ffmpeg_burn_subtitles/Screen.png";
 fi
 if [ ! -f 720p.mp4 ]; then
 ffmpeg -loop 1 -i Screen.png -c:v libx264 -t 4 -pix_fmt yuv420p -vf scale=1280:720 720p.mp4
@@ -21,7 +24,7 @@ if [ ! -f 360p.mp4 ]; then
 ffmpeg -loop 1 -i Screen.png -c:v libx264 -t 4 -pix_fmt yuv420p -vf scale=640:360 360p.mp4
 fi
 if [ ! -f 18px.srt ]; then
-    curl -o '18px.srt' "http://raw.githubusercontent.com/gubnota/fenki_shell_helpers/master/ffmpeg/ffmpeg_burn_subtitles/18px.srt";
+    curl -O "https://raw.githubusercontent.com/gubnota/fenki_shell_helpers/master/ffmpeg/ffmpeg_burn_subtitles/18px.srt";
 fi
 ffmpeg -i 720p.mp4 -vf "subtitles=18px.srt" 720p_sub.mp4
 ffmpeg -i 360p.mp4 -vf "subtitles=18px.srt" 360p_sub.mp4
